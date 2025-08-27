88836831222be9ad461fd361ce96e15fb38b8bed
Angelo Rios <angelorios@live.com>
2025-08-27 17:47:44 +0200

    wip: signal_handler

--- src/server.c
+++ src/server.c
@@ -6,18 +6,28 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 19:06:42 by angrios           #+#    #+#             */
-/*   Updated: 2025/08/27 13:45:28 by angrios          ###   ########.fr       */
+/*   Updated: 2025/08/27 17:32:29 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+char	g_char = 0;
+int		g_bit_counter = 0;
+
 void	signal_handler(int signal)
 {
 	if (signal == SIGUSR1)
-		ft_printf("Señal 1 recibida\n");
+		g_char = (g_char << 1);
 	else if (signal == SIGUSR2)
-		ft_printf("Señal 2 recibida\n");
+		g_char = (g_char << 1) | 1;
+	g_bit_counter++;
+	if (g_bit_counter == 8)
+	{
+		write(1, &g_char, 1);
+		g_char = 0;
+		g_bit_counter = 0;
+	}
 }
 
 int	main(void)
@@ -33,6 +43,8 @@ int	main(void)
 		return (EXIT_FAILURE);
 	ft_printf("Server PID: %d\n", getpid());
 	while (1)
+	{
 		pause();
+	}
 	return (EXIT_SUCCESS);
 }
