b6f4c87b93352e9f23088f29424c613a816cfc39
Angelo Rios <angelorios@live.com>
2025-08-29 21:01:12 +0200

    add: finish mandatory part

--- src/client.c
+++ src/client.c
@@ -6,11 +6,11 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 19:08:58 by angrios           #+#    #+#             */
-/*   Updated: 2025/08/28 20:45:49 by angrios          ###   ########.fr       */
+/*   Updated: 2025/08/29 20:19:17 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "libft.h"
 #include <time.h>
 
 static void	ft_usleep(int microseconds)
@@ -22,50 +22,59 @@ static void	ft_usleep(int microseconds)
 	nanosleep(&ts, NULL);
 }
 
-int	send_signal(int server_pid, int bit)
+static void	send_bit(int server_pid, int bit)
 {
-	if (bit == 0)
-		kill(server_pid, SIGUSR1);
-	else if (bit == 1)
-		kill(server_pid, SIGUSR2);
-	ft_usleep(100);
-	return (0);
+	if (bit == 1)
+	{
+		if (kill(server_pid, SIGUSR2) == -1)
+		{
+			ft_printf("ERROR: could not send SIGUSR2 to PID %d\n", server_pid);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (bit == 0)
+	{
+		if (kill(server_pid, SIGUSR1) == -1)
+		{
+			ft_printf("ERROR: could not send SIGUSR1 to PID %d\n", server_pid);
+			exit(EXIT_FAILURE);
+		}
+	}
+	ft_usleep(500);
 }
 
-int	send_char(int server_pid, char chr)
+static void	process_char(int server_pid, char chr)
 {
-	int	i;
 	int	bit;
 
-	i = 7;
-	while (i >= 0)
+	bit = 0;
+	while (bit < 8)
 	{
-		bit = (chr >> i) & 1;
-		send_signal(server_pid, bit);
-		i--;
+		send_bit(server_pid, (chr >> bit) & 1);
+		bit++;
 	}
-	
-	return (0);
 }
 
 int main(int argc, char *argv[])
 {
 	int		i;
-	int		pid;
+	char	*msg;
+	int		server_pid;
 
 	if (argc != 3)
 	{
-		ft_printf("Usage: %s <SERVER_PID> <MESSAGE>\n", argv[0]);
+		ft_printf("USAGE: %s <server pid> <message>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
+	server_pid = ft_atoi(argv[1]);
+	msg = argv[2];
 	i = 0;
-	pid = ft_atoi(argv[1]);
-	while (argv[2][i] != '\0')
+	while (msg[i])
 	{
-		send_char(pid, argv[2][i]);
+		process_char(server_pid, msg[i]);
 		i++;
 	}
-	send_char(pid, '\0');
-	ft_printf("Message sent succesfully!\n");
+	process_char(server_pid, '\0');
+	ft_printf("\nMessage sent successfuly!\n");
 	return (EXIT_SUCCESS);
 }
--- src/server.c
+++ src/server.c
@@ -6,19 +6,21 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 19:06:42 by angrios           #+#    #+#             */
-/*   Updated: 2025/08/28 21:32:57 by angrios          ###   ########.fr       */
+/*   Updated: 2025/08/29 20:19:07 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
-
+#include "libft.h"
+/*
 static int	store_message(char **msg, char new_char)
 {
 	char	*temp_msg;
 	char	str[2];
 
 	str[0] = new_char;
-	str[1] = '\0';	
+	str[1] = '\0';
+	if (*msg == NULL)
+		*msg = ft_strdup("");
 	temp_msg = ft_strjoin(*msg, str);
 	if (!temp_msg)
 		return (1);
@@ -29,9 +31,9 @@ static int	store_message(char **msg, char new_char)
 
 void	signal_handler(int signal)
 {
-	static char		chr;
-	static int		bit_counter;
-	static char		*msg;
+	static char		chr = 0;
+	static int		bit_counter = 0;
+	static char		*msg = NULL;
 
 	if (signal == SIGUSR1)
 		chr = (chr << 1);
@@ -40,8 +42,15 @@ void	signal_handler(int signal)
 	bit_counter++;
 	if (bit_counter == 8)
 	{
-		// write(1, &chr, 1);
-		store_message(&msg, chr);
+		if (store_message(&msg, chr) != 0)
+			return ;
+		if (chr == '\0')
+		{
+			write(STDOUT_FILENO, msg, ft_strlen(msg));
+			write(STDOUT_FILENO, "\n", 1);
+			free(msg);
+			msg = NULL;
+		}
 		chr = 0;
 		bit_counter = 0;
 	}
@@ -59,9 +68,85 @@ int	main(void)
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
 		return (EXIT_FAILURE);
 	ft_printf("Server PID: %d\n", getpid());
+	sleep(1);
 	while (1)
 	{
 		pause();
 	}
+	return (EXIT_SUCCESS);
+}
+*/
+
+static char	*g_message = NULL;
+
+static void	append_char(char chr)
+{
+	char	*temp;
+	char	str[2];
+
+	str[0] = chr;
+	str[1] = '\0';
+	if (!g_message)
+		g_message = ft_strdup("");
+	if (!g_message)
+	{
+		ft_printf("ERROR: memory allocation failed (append_char, !g_message)!");
+		exit(EXIT_FAILURE);
+	}
+	temp = ft_strjoin(g_message, str);
+	if (!temp)
+	{
+		ft_printf("ERROR: memory allocation failed (append_char, !temp)!");
+		exit(EXIT_FAILURE);
+	}
+	free(g_message);
+	g_message = temp;
+}
+
+static void	handle_char(char chr)
+{
+	if (chr == '\0')
+	{
+		ft_printf("%s\n", g_message);
+		free(g_message);
+		g_message = NULL;
+	}
+	else
+		append_char(chr);
+}
+
+static void	signal_handler(int signal)
+{
+	static char	current_char = 0;
+	static int	bit_counter = 0;
+
+	if (signal == SIGUSR2)
+		current_char |= (1 << bit_counter);
+	bit_counter++;
+	if (bit_counter == 8)
+	{
+		handle_char(current_char);
+		current_char = 0;
+		bit_counter = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	ft_printf("Server PID: %d\n", getpid());
+	g_message = NULL;
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		ft_printf("ERROR: sigaction failed!\n");
+		return (EXIT_FAILURE);
+	}
+	while (1)
+		pause();
 	return (EXIT_SUCCESS);
 }
